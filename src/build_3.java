b611261a9ef5f7b43f3d37c5b2311f68cf4f592d
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-01-28 22:34:30 +0100

    New test files

---
+++ src/build_3.java
@@ -0,0 +1,14 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+
+        public class build_3 {
+            public static void main(String[] args) {
+
+        
+        int c = a + b;
+        
+            }
+        }
+        
