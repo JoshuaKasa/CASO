99f02ca77b87aa787103c6eb7c9d30aba6f7c78f
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-01-30 00:11:27 +0100

    Java files

---
+++ src/test_4.java
@@ -0,0 +1,22 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+
+        public class test_4 {
+            public static void main(String[] args) {
+
+        
+        int a = 10;
+        
+        int b = 15;
+        
+        if (a + 1 - b > 0) {
+        
+        int c = a + b;
+        
+        }
+        
+            }
+        }
+        
