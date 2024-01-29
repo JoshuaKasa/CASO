e157835797f0009596fd778ca6b03e29af84ab8d
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-01-29 01:18:49 +0100

    Updated tests

---
+++ src/test_2.java
@@ -0,0 +1,38 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+
+        public class test_2 {
+            public static void main(String[] args) {
+
+        
+        int a = 10;
+        
+                if (a == 10) {
+                
+        int b = 15;
+        
+        int z = 15;
+        
+                if (z == 5) {
+                
+        int g = 69;
+        
+                }
+                
+                }
+                
+                else if (a > 10) {
+                
+        int c = 20;
+        
+                }
+                
+                else {
+                
+            }
+            
+            }
+        }
+        
