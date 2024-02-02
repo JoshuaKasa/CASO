33bacb59c1c0787f0a41f63c60cca50a4ffa82f2
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-02 21:22:15 +0100

    New test files for function calling and elsif

---
+++ src/build/test_5.java
@@ -0,0 +1,19 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+
+        public class test_5 {
+            public static void main(String[] args) {
+
+        
+        public static int fibonacci(
+        int n) {
+
+        return 1;
+        
+        }
+        
+            }
+        }
+        
