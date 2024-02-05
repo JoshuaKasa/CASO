21050743fbcfb2b2f6e8ca28c804b96f41e16de4
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-06 00:14:39 +0100

    Create caso_types.py

---
+++ src/caso_types.py
@@ -0,0 +1,10 @@
+conversion_table = {
+    'Int': 'int',
+    'Float': 'float',
+    'Str': 'String',
+    'Bool': 'boolean',
+    'Any': 'Object',
+    'List': 'ArrayList',
+    'Empty': 'void',
+    'const': 'final',
+}
