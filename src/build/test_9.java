e8a36f963fd60257dd17e9395c16d0bc568388f1
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-11 22:53:11 +0100

    Added class test

---
+++ src/build/test_9.java
@@ -0,0 +1,28 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+
+        public class test_9 {
+        
+            public static void main(String[] args) {
+
+        
+        public class Person {
+        
+            string name;
+            
+            int age;
+            
+        public static empty my_informations(
+        ) {
+
+        int a = 10;
+        
+        }
+        
+        }
+        
+            }
+        }
+        
