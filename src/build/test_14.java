d572e239749ac552a6078f1afd9a7e45cdc5b310
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-18 23:18:51 +0100

    Added new tests

---
+++ src/build/test_14.java
@@ -0,0 +1,106 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class test_14 {
+        
+            public static void main(String[] args) {
+
+        
+public class Veicolo {
+
+            public string marca;
+            
+            public string modello;
+            
+            public int anno_produzione;
+            
+            public int cilindrata;
+            
+            public int prezzo;
+            
+        public Veicolo (
+        string marca, string modello, int anno_produzione, int cilindrata, int prezzo) {
+
+            this.marca = marca;
+            
+            this.modello = modello;
+            
+            this.anno_produzione = anno_produzione;
+            
+            this.cilindrata = cilindrata;
+            
+            this.prezzo = prezzo;
+            
+        }
+        
+        public static bool equals(
+        int v) {
+
+        bool res = 0;
+        
+        return res;
+        
+        }
+        
+            public string get_marca() {
+                return this.marca;
+            }
+
+            public void set_marca(string marca) {
+                this.marca = marca;
+            }
+        
+            public string get_modello() {
+                return this.modello;
+            }
+
+            public void set_modello(string modello) {
+                this.modello = modello;
+            }
+        
+            public int get_anno_produzione() {
+                return this.anno_produzione;
+            }
+
+            public void set_anno_produzione(int anno_produzione) {
+                this.anno_produzione = anno_produzione;
+            }
+        
+            public int get_cilindrata() {
+                return this.cilindrata;
+            }
+
+            public void set_cilindrata(int cilindrata) {
+                this.cilindrata = cilindrata;
+            }
+        
+            public int get_prezzo() {
+                return this.prezzo;
+            }
+
+            public void set_prezzo(int prezzo) {
+                this.prezzo = prezzo;
+            }
+        
+        @Override
+        public String toString() {
+            return "Veicolo(" +
+        
+            "marca=" + this.marca +
+            
+            "modello=" + this.modello +
+            
+            "anno_produzione=" + this.anno_produzione +
+            
+            "cilindrata=" + this.cilindrata +
+            
+            "prezzo=" + this.prezzo +
+            
+            ")";
+        }}
+        
+            }
+        }
+        
