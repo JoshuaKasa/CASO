a51774e4721c7f39f2bbb35cfc06626c028acca3
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-19 22:57:13 +0100

    Added future test cases

---
+++ src/build/test_15.java
@@ -0,0 +1,47 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class test_15 {
+        
+            public static void main(String[] args) {
+
+        
+        if (1 > 0) {
+        
+        }
+        
+        else if (1 > 0) {
+        
+        }
+        
+        else {
+        
+        }
+        
+        if (a > 0) {
+        
+        if (a > 0) {
+        
+        }
+        
+        else if (a > 0) {
+        
+        }
+        
+        int a = 1;
+        
+        else {
+        
+        }
+        
+        }
+        
+        else if (a > 0) {
+        
+        }
+        
+            }
+        }
+        
