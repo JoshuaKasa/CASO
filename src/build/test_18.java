12bfc80ec22699dfabff57264d2049c795c3ac69
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-22 21:47:49 +0100

    New test build

---
+++ src/build/test_18.java
@@ -0,0 +1,48 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class test_18 {
+        
+            public static void main(String[] args) {
+
+        
+public class Hello {
+
+            public INT age;
+            
+        public Hello (
+        INT age) {
+
+            this.age = age;
+            
+        }
+        
+        public static String say_hello(
+        ) {
+
+        return "Hello, World! I am " + age + " years old.";
+        
+        }
+        
+            public INT get_age() {
+                return this.age;
+            }
+
+            public void set_age(INT age) {
+                this.age = age;
+            }
+        
+        @Override
+        public String toString() {
+            return "Hello(" +
+        
+            "age=" + this.age +
+            
+            ")";
+        }}
+        
+            }
+        }
+        
