6cef60a65b06dde3e3422e162f4fdce402308b7f
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-24 19:46:22 +0100

    Test files

---
+++ src/build/test_19.java
@@ -0,0 +1,15 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class test_19 {
+        
+            public static void main(String[] args) {
+
+        
+        int a = 1;
+        
+            }
+        }
+        
