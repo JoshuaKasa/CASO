bde936209eb57a1da7b61d7ed41b2350470714e3
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-03-03 21:34:05 +0100

    Create test_20.java

---
+++ src/build/test_20.java
@@ -0,0 +1,19 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+            import standard;
+            
+        public class test_20 {
+        
+            public static void main(String[] args) {
+
+        
+        String name = "Tommaso";
+        
+        standard.print_line(
+        "Hello, World!" + name);
+            }
+        }
+        
