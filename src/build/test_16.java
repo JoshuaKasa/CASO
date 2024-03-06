a7c933ef59a989f4b57a63d053d47b649cdc3faa
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-03-06 23:30:38 +0100

    Changed test files

--- src/build/test_16.java
+++ src/build/test_16.java
@@ -1,1 +1,40 @@
 
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class test_16 {
+        
+        System.out.println(s);
+        
+        public static  void print_line(
+        String s) {
+
+        }
+        
+        System.out.print(prompt);
+	return new java.util.Scanner(System.in).nextLine();
+        
+        public static  String read_line(
+        String prompt) {
+
+        }
+        
+        System.out.print(prompt);
+	return new java.util.Scanner(System.in).nextInt();
+        
+        public static  int read_int(
+        String prompt) {
+
+        }
+        
+            public static void main(String[] args) {
+
+        
+        int STD_SUCCESS = 0;
+        
+        int STD_FAILURE = 1;
+        
+            }
+        }
+        
--- src/build/test_20.java
+++ src/build/test_20.java
@@ -3,16 +3,28 @@
         import java.util.Scanner;
         import java.util.ArrayList;
         
-            import libraries.standard; 
+            import libraries.module_name.STD_SUCCESS; 
+            
+            import libraries.module_name.STD_FAILURE; 
+            
+            import libraries.module_name.print_line; 
+            
+            import libraries.module_name.read_line; 
+            
+            import libraries.module_name.; 
             
         public class test_20 {
         
             public static void main(String[] args) {
 
         
+        int STD_SUCCESS = 0;
+        
+        int STD_FAILURE = 1;
+        
         String name = "Tommaso" + " " + "Santamaria";
         
-        standard.print_line(
+            print_line(
         "Hello, World!" + name);
             }
         }
