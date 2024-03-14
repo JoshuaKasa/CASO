d0c5c6bc6eaf102e6f3f6f83a7bd25fae4d63993
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-03-14 23:52:36 +0100

    Testing builds

---
+++ src/build/libraries/List.java
@@ -0,0 +1,18 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class List {
+        
+            public static void main(String[] args) {
+
+        
+                int length = 0;
+                
+                int[] array_data =
+                {};
+
+            }
+        }
+        
--- src/build/test_1.java
+++ src/build/test_1.java
@@ -2,25 +2,30 @@
         // This is the general header template for the transpiled code
         import java.util.Scanner;
         import java.util.ArrayList;
-
+        
         public class test_1 {
+        
             public static void main(String[] args) {
 
         
-        int x = 10;
-        
-        int y = 20;
-        
-        int z = x + y;
-        
+                int x = 10;
+                
+                int y = 20;
+                
+                int z = x + y;
+                
         x = 2;
         
         z = x + y;
         
         x = z + 1;
         
-        list[list[int]] a = [1, 2, 3, 4];
-        
+                int[] a =
+                {1 + 2, x + z - x, 3, 4};
+
+                int[][] b =
+                {{1, 2}, {3, 4}};
+
             }
         }
         
