7c7f38d805da2d1fd206a48789310e9c4d467aa9
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-03-15 23:43:24 +0100

    Builds

--- src/build/test_16.java
+++ src/build/test_16.java
@@ -5,36 +5,13 @@
         
         public class test_16 {
         
-        System.out.println(s);
-        
-        public static  void print_line(
-        String s) {
-
-        }
-        
-        System.out.print(prompt);
-	return new java.util.Scanner(System.in).nextLine();
-        
-        public static  String read_line(
-        String prompt) {
-
-        }
-        
-        System.out.print(prompt);
-	return new java.util.Scanner(System.in).nextInt();
-        
-        public static  int read_int(
-        String prompt) {
-
-        }
-        
             public static void main(String[] args) {
 
         
-        int STD_SUCCESS = 0;
-        
-        int STD_FAILURE = 1;
-        
+                int STD_SUCCESS = 0;
+                
+                int STD_FAILURE = 1;
+                
             }
         }
         
--- src/build/test_18.java
+++ src/build/test_18.java
@@ -8,8 +8,6 @@
             public static void main(String[] args) {
 
         
-        Hello.try_object(
-        );
             }
         }
         
