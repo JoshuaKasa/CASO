2caa73b86d20f9033c578e11a20643cf3e47b536
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-04-07 21:47:05 +0200

    Builds

---
+++ src/build/showcase_1.java
@@ -0,0 +1,17 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+            import libraries.standard;
+            
+        public class showcase_1 {
+        
+            public static void main(String[] args) {
+
+        
+        standard.print_line(
+        "Hello, World!");
+            }
+        }
+        
---
+++ src/build/showcase_2.java
@@ -0,0 +1,45 @@
+
+        // This is the general header template for the transpiled code
+        import java.util.Scanner;
+        import java.util.ArrayList;
+        
+        public class showcase_2 {
+        
+                int n = 13;
+                
+            check_prime(
+        n);
+        public  void main(
+        ) {
+
+        }
+        
+        if (n < 2) {
+        
+        standard.print_line(
+        "Not prime");
+        }
+        
+        for (int i = 2; i <= n / 2; i++) {
+        
+        if (n % i == 0) {
+        
+        standard.print_line(
+        "Not prime");
+        }
+        
+        }
+        
+        standard.print_line(
+        "Prime");
+        public  void check_prime(
+        int n) {
+
+        }
+        
+            public static void main(String[] args) {
+
+        
+            }
+        }
+        
